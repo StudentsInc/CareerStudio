37ddbf7947f01220a5e82490365f5bfe94e5cbe7
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-07-29 01:25:55 +0300

    custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "background-color": " #131320",
+        "blue-main": "#3D3BFF",
+        "blue-text": "#4544DC",
+        "blue-card-bg": "#1A1830",
+        "blue-card-bg-dark": "#0A0A10",
+        "blue-progress": "#6097FF",
+        "purple-color": "#7860BD",
+        "yellow-color": "#FFAB6B",
+        "red-error": "#EF4444",
+        "green-success": "#29A35A",
+        "gray-progress-bar": "#6B7280",
+        "gray-text": "#D1D5DB",
+      },
+      screens: {
+        xs: "480px",
+      },
+    },
   },
   plugins: [],
 };
