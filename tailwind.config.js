b7fdd721b1eeaa13c23bedc7fc31e0f3f4378da5
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-08-07 23:30:41 +0300

    tailwind base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.25rem",
+        sm: "4rem",
+        lg: "6rem",
+      },
+    },
     extend: {
+      fontSize: {
+        heading: "3.75rem", //60px
+        sub1: "2.063rem", // 33px
+        sub2: "1.563rem", // 25px
+        body: "1rem", // 16px
+        "body-mobile": "0.875rem", // 14px
+      },
       colors: {
-        "background-color": " #131320",
+        "background-color": "#121826", // "#131320",
         "blue-main": "#3D3BFF",
         "blue-text": "#4544DC",
         "blue-card-bg": "#1A1830",
@@ -19,6 +34,12 @@ export default {
       },
       screens: {
         xs: "480px",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+      boxShadow: {
+        button: "4px 4px 24px 0px rgba(74, 73, 224, 0.51);",
       },
     },
   },
