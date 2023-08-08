60ef5b4b17062b60a5dd75702a16ce415d9a1e17
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-08-09 02:47:04 +0300

    home and develop routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home, Dashboard } from "./pages"
+import { MainLayout } from "./layout/MainLayout"
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<>Hi</>} />
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
