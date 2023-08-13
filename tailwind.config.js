09ee438c0996dc046f38ee72d23eb872cb189ee7
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-08-13 23:49:17 +0300

    add colors in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,36 @@ export default {
         inter: ['Inter', 'sans-serif'],
         poppins: ['Poppins', 'sans-serif'],
       },
+      colors: {
+        blue: {
+          "brand": "#3D3BFF",
+          "secondary": "#4746DE",
+          "text": "#7594FB",
+          "progress": "#6097FF",
+          "gradient": "#6665FF",
+          "card-bg": "#1A1830",
+          "card-bg-dark": "#0A0A10",
+        },
+        yellow: {
+          "main": "#F8A76F",
+          "secondary": "#FDAA6D",
+        },
+        purple: {
+          "main": "#7860BD",
+          "text-pct": "#C67BFF",
+        },
+        gray: {
+          "bg-progress": "#6B7280",
+          "bg-circle": "#707070",
+          "bg-card": "#41495F",
+          "text": "#fff",
+          "bg-input": "#252A40",
+          "bg-dark-input": "#232332",
+          "disabled": "#2D3748"
+        },
+        "red": "#EF4444",
+        "green": "#29A35A",
+      },
       boxShadow: {
         'dark-inset': '0 4px 6px -1px rgba(0, 0, 0, 0.1) inset, 0 2px 4px -1px rgba(0, 0, 0, 0.06) inset',
         'light-inset': '0px 4.21px 4.21px 0px rgba(249,247, 243, 0.25) inset, 0px -4.21px 4.21px 0px rgba(0, 0, 0, 0.25) inset',
