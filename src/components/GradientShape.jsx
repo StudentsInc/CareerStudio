69f375317b3f638867699926a72d4d87915de851
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-08-13 23:56:17 +0300

    create gradientShape component

---
+++ src/components/GradientShape.jsx
@@ -0,0 +1,22 @@
+const GradientShape = (props) => {
+	const defaultGradients = [
+		"circle at center, var(--yellow-color), transparent 70%",
+		"circle at bottom, var(--blue-main), transparent 100%",
+	];
+
+	const gradientStyle =
+		props.gradients && props.gradients.length > 0
+			? props.gradients
+					.map((gradient) => `radial-gradient(${gradient})`)
+					.join(",")
+			: defaultGradients
+					.map((gradient) => `radial-gradient(${gradient})`)
+					.join(",");
+	return (
+		<div
+			className={`absolute transform -rotate-20 bg-cover bg-no-repeat rounded-full z-[-1] ${props.circleStyle}`}
+			style={{ backgroundImage: gradientStyle }}></div>
+	);
+};
+
+export default GradientShape;
