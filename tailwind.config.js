22758ea0a62069041ecc4fa9bb61a1e6d24cde55
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-08-13 22:04:26 +0300

    add container && boxShadow colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,21 @@ export default {
         inter: ['Inter', 'sans-serif'],
         poppins: ['Poppins', 'sans-serif'],
       },
+      boxShadow: {
+        'dark-inset': '0 4px 6px -1px rgba(0, 0, 0, 0.1) inset, 0 2px 4px -1px rgba(0, 0, 0, 0.06) inset',
+        'light-inset': '0px 4.21px 4.21px 0px rgba(249,247, 243, 0.25) inset, 0px -4.21px 4.21px 0px rgba(0, 0, 0, 0.25) inset',
+        'light-ouset': '4px 4px 24px 0px rgba(74, 73, 224, 0.51)'
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '3rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
     },
   },
   plugins: [],
