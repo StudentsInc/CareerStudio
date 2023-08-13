96893b857eed77c7bbebe00e9ccdd8760784db73
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-08-13 18:19:17 +0300

    setup browserRouter,folders and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import AppLayout from "./AppLayout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ForgotPass from "./pages/ForgotPass";
+import ResetPass from "./pages/ResetPass";
 function App() {
-	return <div></div>;
+	return (
+		<AppLayout>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/signUp' element={<Signup />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/forgotPassword' element={<ForgotPass />} />
+				<Route path='/resetPassword' element={<ResetPass />} />
+			</Routes>
+		</AppLayout>
+	);
 }
 
 export default App;
