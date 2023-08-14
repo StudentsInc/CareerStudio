98c4c9a93a8c23fc15229a79ff8dbd160b1e683d
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-08-14 05:12:10 +0300

    create carousel section with its data,style and images

---
+++ src/assets/homeData.js
@@ -0,0 +1,34 @@
+import { carousel1Png, carousel1Webp, carousel2Png, carousel2Webp, carousel3Png, carousel3Webp } from "./images"
+const homeData = {
+	"carouselContents": [
+		{
+			"title": "Know Your Self",
+			"description": "Love problem-solving but not big on socializing? You could be an engineer! More into storytelling than equations? Journalism might be your calling! Take our personality assessment now and get matched with careers that fit YOU, not the other way around.",
+			"number": 1,
+			"buttonText": "Start Here",
+			"imagePng": carousel1Png,
+			"imageWebp": carousel1Webp,
+			"alt": "Illustration representing the idea of knowing oneself and finding suitable careers through personality assessment."
+		},
+		{
+			"title": "EXPLORE YOUR OPTIONS",
+			"description": "Now that we've unlocked the secrets of your personality, it's time to match you with careers that suit you best. But we're not talking about your run-of-the-mill career suggestions. We're talking about a carefully curated list of careers that align with your personality traits and interests.",
+			"number": 2,
+			"imagePng": carousel2Png,
+			"imageWebp": carousel2Webp,
+			"buttonText": "Start now",
+			"image": "/src/assets/homePage/images/carousel-img2.png",
+			"alt": "Illustration depicting the process of exploring career options based on personality traits and interests."
+		},
+		{
+			"title": "Start your journey",
+			"description": "Armed with knowledge and a clear direction, it's time to take the leap. We provide the tools and resources you need to chart your course. Whether it's finding the right college, scholarship, or internship, we are here to help you kickstart your journey. Don't just dream about your future—start building it",
+			"number": 3,
+			"buttonText": "click me already",
+			"imagePng": carousel3Png,
+			"imageWebp": carousel3Webp,
+			"alt": "Illustration symbolizing the beginning of a journey with tools and resources to shape one's future."
+		}
+	]
+}
+export default homeData;
