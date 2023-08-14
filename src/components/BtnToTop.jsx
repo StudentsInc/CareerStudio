19dcf8b516fbf89ee5643904483990b38b579450
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-08-14 05:14:57 +0300

    edit colors

--- src/components/BtnToTop.jsx
+++ src/components/BtnToTop.jsx
@@ -30,7 +30,7 @@ const BtnToTop = () => {
 		<>
 			{isVisible && (
 				<button
-					className='fixed  bottom-8 xs-bottom-6 bg-gradient-to-b from-blue-main to-yellow-color z-10 pointer right-3 xs:right-6 bg-blue-500 hover:bg-blue-600 text-white px-2 py-2 rounded-full shadow'
+					className='fixed  bottom-8 xs-bottom-6 bg-gradient-to-b from-blue-brand to-yellow-main z-10 pointer right-3 xs:right-6 bg-blue-500 hover:bg-blue-600 text-white px-2 py-2 rounded-full shadow'
 					onClick={scrollToTop}>
 					<FiArrowUp size={20} />
 				</button>
