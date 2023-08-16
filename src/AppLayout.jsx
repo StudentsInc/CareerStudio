510f429b74a27547a8523e86ea69ff95a6c70874
MusafaiHUB <abuobeidmustafa1@gmail.com>
2023-08-16 19:04:39 +0300

    Remove navbar from reset & forgot pages

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -3,17 +3,22 @@ import Navbar from "./components/Navbar";
 import { useLocation } from "react-router-dom";
 
 const AppLayout = ({ children }) => {
-	const location = useLocation();
-	const excludeNavbarPaths = ["/login", "/signUp"]; // Specify paths where Navbar should be excluded
-	const shouldShowNavbar = !excludeNavbarPaths.includes(location.pathname);
+  const location = useLocation();
+  const excludeNavbarPaths = [
+    "/login",
+    "/signUp",
+    "/resetPassword",
+    "/forgotPassword",
+  ]; // Specify paths where Navbar should be excluded
+  const shouldShowNavbar = !excludeNavbarPaths.includes(location.pathname);
 
-	return (
-		<div className='relative '>
-			<BtnToTop />
-			{shouldShowNavbar && <Navbar />}
-			{children}
-		</div>
-	);
+  return (
+    <div className="relative ">
+      <BtnToTop />
+      {shouldShowNavbar && <Navbar />}
+      {children}
+    </div>
+  );
 };
 
 export default AppLayout;
