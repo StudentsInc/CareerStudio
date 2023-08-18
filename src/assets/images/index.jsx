cd7d93416934d8cd0ede8a6c378f4720f2701f82
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-08-19 02:13:17 +0300

    change wavy backgrounds for a good quality and update index file

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -1,32 +1,30 @@
-export { default as logo } from "./logo.svg"
-export { default as carousel1Png } from "./homePage/carousel/carousel-img1.png"
-export { default as carousel1Webp } from "./homePage/carousel/carousel-img1.webp"
-export { default as carousel2Png } from "./homePage/carousel/carousel-img2.png"
-export { default as carousel2Webp } from "./homePage/carousel/carousel-img2.webp"
-export { default as carousel3Png } from "./homePage/carousel/carousel-img3.png"
-export { default as carousel3Webp } from "./homePage/carousel/carousel-img3.webp"
-export { default as ellipse1 } from "./homePage/ellipse/ellipse1.svg"
-export { default as ellipse2 } from "./homePage/ellipse/ellipse2.svg"
-export { default as ellipse3 } from "./homePage/ellipse/ellipse3.svg"
-export { default as ellipse4 } from "./homePage/ellipse/ellipse4.svg"
-export { default as ellipse5 } from "./homePage/ellipse/ellipse5.svg"
-export { default as ellipse6 } from "./homePage/ellipse/ellipse6.svg"
-export { default as wavyShape } from "./homePage/wavy/wavyShape.png"
-export { default as wavyShapeWeb } from "./homePage/wavy/wavyShape.webp"
-export { default as person1 } from "./homePage/people/person1.png"
-export { default as person2 } from "./homePage/people/person2.png"
-export { default as person3 } from "./homePage/people/person3.png"
-export { default as person4 } from "./homePage/people/person4.png"
-export { default as person5 } from "./homePage/people/person5.png"
-export { default as person6 } from "./homePage/people/person6.png"
-export { default as person7 } from "./homePage/people/person7.png"
+export { default as logo } from "./logo.svg";
+export { default as carousel1Png } from "./homePage/carousel/carousel-img1.png";
+export { default as carousel1Webp } from "./homePage/carousel/carousel-img1.webp";
+export { default as carousel2Png } from "./homePage/carousel/carousel-img2.png";
+export { default as carousel2Webp } from "./homePage/carousel/carousel-img2.webp";
+export { default as carousel3Png } from "./homePage/carousel/carousel-img3.png";
+export { default as carousel3Webp } from "./homePage/carousel/carousel-img3.webp";
+export { default as ellipse1 } from "./homePage/ellipse/ellipse1.svg";
+export { default as ellipse2 } from "./homePage/ellipse/ellipse2.svg";
+export { default as ellipse3 } from "./homePage/ellipse/ellipse3.svg";
+export { default as ellipse4 } from "./homePage/ellipse/ellipse4.svg";
+export { default as ellipse5 } from "./homePage/ellipse/ellipse5.svg";
+export { default as ellipse6 } from "./homePage/ellipse/ellipse6.svg";
+export { default as wavyShape } from "./homePage/wavy/wavyShape.png";
+export { default as wavyShapeWeb } from "./homePage/wavy/wavyShape.webp";
+export { default as person1 } from "./homePage/people/person1.png";
+export { default as person2 } from "./homePage/people/person2.png";
+export { default as person3 } from "./homePage/people/person3.png";
+export { default as person4 } from "./homePage/people/person4.png";
+export { default as person5 } from "./homePage/people/person5.png";
+export { default as person6 } from "./homePage/people/person6.png";
+export { default as person7 } from "./homePage/people/person7.png";
 
-// dashboard 
-export { default as interest1 } from "./dashboard/icons/interest1.png"
-export { default as interest2 } from "./dashboard/icons/interest2.png"
-export { default as interest3 } from "./dashboard/icons/interest3.png"
-export { default as interest4 } from "./dashboard/icons/interest4.png"
-export { default as interest5 } from "./dashboard/icons/interest5.png"
-export { default as interest6 } from "./dashboard/icons/interest6.png"
-
-
+// dashboard
+export { default as interest1 } from "./dashboard/icons/interest1.svg";
+export { default as interest2 } from "./dashboard/icons/interest2.svg";
+export { default as interest3 } from "./dashboard/icons/interest3.svg";
+export { default as interest4 } from "./dashboard/icons/interest4.svg";
+export { default as interest5 } from "./dashboard/icons/interest5.svg";
+export { default as interest6 } from "./dashboard/icons/interest6.svg";
