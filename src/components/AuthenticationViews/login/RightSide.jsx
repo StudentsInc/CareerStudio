5a718921f99fadd90a54cb7dbb1836ed1396a93f
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-08-21 19:15:49 +0300

    create login page with its components and styling

---
+++ src/components/AuthenticationViews/login/RightSide.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { logo } from "../../../assets/images";
+import { Link } from "react-router-dom";
+import Form from "./Form";
+import Logo from "../../Logo";
+
+const RightSide = () => {
+	const [formValues, setFormValues] = useState({});
+
+	const handleChange = (e) => {
+		setFormValues({ ...formValues, [e.target.name]: e.target.value });
+	};
+
+	console.log(formValues);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log("Form submitted successfully");
+	};
+	return (
+		<div className='w-full h-full py-[2.9rem] py-none bg-transparent flex justify-center items-center flex-col'>
+			<div className='text-white md:hidden flex justify-center items-center  mb-20'>
+				<Logo
+					txt1={"Career"}
+					txt2={"Studio"}
+					txtStyle={"font-bold text-[1.875rem]"}
+					imgStyle={"h-[44px]"}
+					imgUrl={logo}
+					logoStyle={"md:hidden flex items-center gap-[12.8px]"}
+				/>
+			</div>
+
+			<div className='container'>
+				<div className='form-content w-[100%] lg:w-[80%] 2xl:w-[60%] mx-auto py-[25px] md:py-[119px]'>
+					<div className='mb-10'>
+						<p className='text-[#BDBDBD]'>Welcome back</p>
+						<h1 className='text-white font-bold text-[26px] xl:text-[2rem]'>
+							Login to your account
+						</h1>
+					</div>
+					<Form handleChange={handleChange} handleSubmit={handleSubmit} />
+					<div className='text-[#616161] text-center'>
+						Don't have an account?{" "}
+						<Link to={"/signUp"}>
+							{" "}
+							<span className='text-[#2B6CB0]'>Join free today</span>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default RightSide;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import LeftSide from "../components/AuthenticationViews/LeftSide";
+import RightSide from "../components/AuthenticationViews/login/RightSide";
+import "./pagesStyle.css";
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<div className='flex min-h-screen'>
+			<LeftSide />
+			<RightSide />
+		</div>
+	);
 };
 
 export default Login;
