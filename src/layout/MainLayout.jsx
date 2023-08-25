b6c2d4546ea1c256e375b0583073527d99529e80
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2023-08-26 02:37:24 +0300

    prevent absolute positioned elem from over flowing

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,11 +2,11 @@ import { Outlet } from "react-router"
 import { Navbar, Footer } from "../components"
 export const MainLayout = () => {
     return (
-        <>
+        <div className="relative overflow-hidden">
             <Navbar />
             <Outlet />
             {/* <Footer /> */}
-        </>
+        </div>
     )
 }
 
